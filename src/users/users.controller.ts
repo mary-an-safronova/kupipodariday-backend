9df90fc9d778c626d2e87ac9f4db2968b5e2fd29
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-11-28 19:25:48 +0300

    in some controllers, guards are installed on the entire controller at once

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,6 +17,7 @@ import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { SearchUserDto } from './dto/search-user.dto';
 import { WishesService } from 'src/wishes/wishes.service';
 
+@UseGuards(JwtGuard) // Гард JwtGuard применен ко всем методам контроллера
 @Controller('users')
 export class UsersController {
   constructor(
@@ -25,56 +26,48 @@ export class UsersController {
   ) {}
 
   // Получаем информацию о себе
-  @UseGuards(JwtGuard)
   @Get('/me')
   async getMe(@Req() req) {
     return await this.usersService.findOne(req.user.id);
   }
 
   // Получаем всех пользователей
-  @UseGuards(JwtGuard)
   @Get()
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   // Получаем пользователя по имени
-  @UseGuards(JwtGuard)
   @Get(':username')
   async findOneByName(@Param('username') username: string) {
     return await this.usersService.findOneByName(username);
   }
 
   // Находим пользователя в поиске по почте или имени
-  @UseGuards(JwtGuard)
   @Post('/find')
   async findMany(@Body() query: SearchUserDto) {
     return await this.usersService.findMany(query);
   }
 
   // Изменяем информацию о себе
-  @UseGuards(JwtGuard)
   @Patch('/me')
   async update(@Req() req, @Body() updateUserDto: UpdateUserDto) {
     return await this.usersService.update(req.user.id, updateUserDto);
   }
 
   // Получаем все свои пожелания
-  @UseGuards(JwtGuard)
   @Get('/me/wishes')
   getMyWishes(@Req() req) {
     return this.wishesService.findUserWishes(req.user.id);
   }
 
   // Получаем пожелания пользователя по его имени
-  @UseGuards(JwtGuard)
   @Get(':username/wishes')
   async getUsernameWishes(@Param('username') username: string) {
     const { id } = await this.usersService.findOneByName(username);
     return this.wishesService.findUserWishes(id);
   }
 
-  @UseGuards(JwtGuard)
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number) {
     return await this.usersService.removeById(id);
